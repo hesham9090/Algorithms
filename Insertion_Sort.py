adc6be84865800a5b982be3a1f2e596b8d76c7b1
Hesham Fawzy <37234887+hesham9090@users.noreply.github.com>
2020-03-25 23:10:30 +0200

    Add files via upload

---
+++ Insertion_Sort.py
@@ -0,0 +1,30 @@
+"""
+Insertion Sort is not fast algorithm because it uses nested loops to sort
+it can be used only for small data
+"""
+
+data = [100,12,90,19,22,8,12]
+data2 = [9]
+data3 = [10,9,8,7,6,5,4,3,2,1]
+
+def insertion_sort(data):
+
+   for j in range(1, len(data)):
+       key = data[j]
+       i = j - 1
+       while (i >= 0) and (data[i] > key):
+           data[i + 1] = data[i]
+           i = i - 1
+       data[i + 1] = key
+   return data
+
+test_case1 = insertion_sort(data)
+test_case2 = insertion_sort(data2)
+test_case3 = insertion_sort(data3)
+print(test_case1)
+print(test_case2)
+print(test_case3)
+
+"""
+Time Complexity Analysis O(n^2)
+"""
